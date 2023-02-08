9f023ce2dd585af28bdeb46e693e1eebfd1637ac
sashatytko <sashatytko@gmail.com>
2023-02-08 21:16:45 +0200

    Added images to the Dice Roller app (5 part)

--- app/src/main/java/edu/tytko/lab1/MainActivity.kt
+++ app/src/main/java/edu/tytko/lab1/MainActivity.kt
@@ -2,10 +2,10 @@ package edu.tytko.lab1
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-//Activity allows the user to roll a dice and see the result value
+// Activity allows the user to roll a dice and see the result value
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +15,28 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener { rollDice() }
     }
 
-    // Roll the dice and update the textView value
+    // Roll the dice and show the proper image
     private fun rollDice() {
+        // Create dice object and make roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+        // Determine which drawable to use based on the dice
+        val diceImage = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Find the dice image view
+        val diceImageView: ImageView = findViewById(R.id.imageView)
+
+        // Update dice image with new dice roll
+        diceImageView.setImageResource(diceImage)
+        // Update dice image description with new dice roll
+        diceImageView.contentDescription = diceRoll.toString()
     }
 }
